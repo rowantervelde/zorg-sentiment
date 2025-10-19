076c820ea2b56928b10a0035a7088ba55f84da05
tervelde.com <info@tervelde.com>
2025-10-19 11:15:37 +0200

    fix: implement dynamic preset configuration for Nuxt to support Playwright tests and add cross-env dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   pages: false,
   ssr: true,
   nitro: {
-    preset: 'netlify'
+    preset: process.env.NUXT_PRESET || 'netlify'
   },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
   css: ['~/assets/styles/main.css'],
